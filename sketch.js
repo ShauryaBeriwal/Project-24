f59536488b393c5cef75551242a33240f258f859
ShauryaBeriwal <76430047+ShauryaBeriwal@users.noreply.github.com>
2021-01-19 14:03:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,paper1;
+var groundObject;
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paper1 = new paper(200,450,40);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+ 
+
+  dustbinObj.display();
+  paper1.display();
+  groundObject.display();
+
+}
+
+function keyPressed() { 
+	if (keyCode === UP_ARROW) { 
+	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85}); } 
+}
